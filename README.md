eea02076c78eeddb06c047e90caedd1263a61bf6
yarenty <yarenty@gmail.com>
2015-04-01 14:35:39 +0100

    initial test of spark

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # spark_cuda
-Heterogenous Cluster - use GPU (CUDA) for highly intensive computing
+Heterogenous Cluster - use GPU (CUDA) for highly intensive computing.
+
+
+## INIT
+Download spark and compile it - I'm using scala 2.11 so need to switch spark to proper one:
+<pre>
+dev/change-version-to-2.11.sh
+mvn -Pyarn -Phadoop-2.4 -Dscala-2.11 -DskipTests clean package
+</pre>
+Test if is working:
+./bin/spark-shell 
+<pre>
+....
+</pre>
+
+## Build this project
+<pre>
+sbt package
+sbt eclipse  // for eclipse
+</pre>
+
+
+## Run it
+<pre>
+../../spark-1.3.0/bin/spark-submit --class "SimpleApp" --master local[4] target/scala-2.11/spark_cuda_2.11-1.0.0.jar
+</pre>
---
+++ src/main/scala/com/yarenty/spark/SimpleApp.scala
@@ -0,0 +1,18 @@
+package com.yarenty.spark
+
+/* SimpleApp.scala */
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "/Users/yarenty/spark-1.3.0/README.md" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+  }
+}
