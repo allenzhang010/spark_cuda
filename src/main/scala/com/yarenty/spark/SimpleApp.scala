18048dba7dba3b99fef952b7f4c1683b2e89b382
yarenty <yarenty@gmail.com>
2015-04-02 13:41:23 +0100

    more fun

--- src/main/scala/com/yarenty/spark/SimpleApp.scala
+++ src/main/scala/com/yarenty/spark/SimpleApp.scala
@@ -14,5 +14,14 @@ object SimpleApp {
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    
+    val data = 1 to 1000
+    
+    val accum = sc.accumulator(0, "My Accumulator")  
+    val distData = sc.parallelize(data).foreach(x => accum += x)
+
+    println("Accumulator: %s".format(accum));
+    
+    
   }
 }
